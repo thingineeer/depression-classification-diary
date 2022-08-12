59b9625444e7d6fd6b7ea6c326a819025fd29e3a
Sabin <sabin5105@gmail.com>
2022-08-12 11:22:35 +0900

    add character classification

--- emotion_classification.py
+++ emotion_classification.py
@@ -5,11 +5,50 @@ import numpy as np
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
+from konlpy.tag import Mecab
 
-model = AutoModelForSequenceClassification.from_pretrained("model_output")
-tokenizer = AutoTokenizer.from_pretrained("tokenizer")
+
+model = AutoModelForSequenceClassification.from_pretrained("model_output_KcElectra")
+tokenizer = AutoTokenizer.from_pretrained("tokenizer_KcElectra")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+mecab = Mecab()
 
+
+def pass_model(tokenzied_sent):
+    with torch.no_grad():
+        output = model(**tokenzied_sent)
+
+    logits = output[0].detach().cpu().numpy()
+    result = np.argmax(logits, axis=-1)
+
+    return result
+
+
+def char_predict(tokenized_sent):
+
+    token = tokenizer.convert_ids_to_tokens([int(char) for char in tokenized_sent["input_ids"][0]][1:-1])
+    pos = mecab.pos(''.join(token))
+
+    for pos_info in pos:
+        if pos_info[1] not in ["JKS","JKC","JKG","JKO","JKB","JKV","JKQ","JX","JC","EP","EF","EC","ETN","ETM","SF","SE","SSO","SSC","SC","SY"]:
+            
+            token_char = tokenizer(
+                pos_info[0],
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=512,
+                add_special_tokens=True,
+            )
+
+            tokenized_sent.to(device)
+
+            result = pass_model(token_char)
+            if result == 0:
+                print(f"악성 : {pos_info[0]}")
+        else:
+            continue
+            
 
 def sentence_predict(sent):
     model.eval()
@@ -25,14 +64,12 @@ def sentence_predict(sent):
 
     tokenized_sent.to(device)
 
-    with torch.no_grad():
-        output = model(**tokenized_sent)
+    result = pass_model(tokenized_sent)
 
-    logits = output[0].detach().cpu().numpy()
-    result = np.argmax(logits, axis=-1)
-
+    # 악성댓글
     if result == 0:
-        return "악성댓글"
+        char_predict(tokenized_sent)
+
     else:
         return "정상댓글"
 
